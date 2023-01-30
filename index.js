4a94bcfb88a054f885f1c9f49abba10b4ddaad6c
tfermoso <tomas.fermoso@gmail.com>
2023-01-30 18:22:29 +0100

    peticiones post

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const path =require('path');
+var bodyparser=require("body-parser");
 const app = express()
 const port = 3000
+
+app.use(bodyparser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
 let options={
@@ -9,6 +12,14 @@ let options={
 }
   res.sendFile("/views/index.html",options);
 })
+
+
+app.post("/",(req,res)=>{
+    console.log(req.body);
+    res.send("recibiendo datos..")
+})
+
+
 app.get("/hola",(req,res)=>{
 res.send("hola que tal");
 })
