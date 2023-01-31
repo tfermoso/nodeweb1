a375dd6dedd7aa4ce370136b6c51fb27b5549216
tfermoso <tomas.fermoso@gmail.com>
2023-01-31 16:12:49 +0100

    mostrando tabla de libros

--- index.js
+++ index.js
@@ -1,36 +1,65 @@
 const express = require('express')
-const path =require('path');
-var mysql=require("mysql");
-var bodyparser=require("body-parser");
+const path = require('path');
+var mysql = require("mysql");
+const fs = require('fs');
+var bodyparser = require("body-parser");
 const app = express()
 const port = 3000
 
 app.use(bodyparser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-let options={
-    root:path.join(__dirname)
-}
-  res.sendFile("/views/index.html",options);
+    let options = {
+        root: path.join(__dirname)
+    }
+    //Conexión a BBDD para leer los libros
+    var conn = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'biblioteca'
+    });
+    conn.connect();
+    conn.query("select * from libros", (err, results, fields) => {
+        let filas = "";
+        for (let index = 0; index < results.length; index++) {
+            const element = results[index];
+            filas += `<tr><td>${element.idlibro}</td><td>${element.codigo}</td>
+        <td>${element.titulo}</td>
+        <td>${element.genero}</td>
+        </tr>`;
+        }
+        try {
+            const data = fs.readFileSync('./views/index.html', 'utf8');
+            let contenido=data.replace("##libros_rows##",filas);
+            res.send(contenido); 
+        } catch (err) {
+            console.error(err);
+            res.send("Error 404");
+        }
+
+    
+    })
+    //res.sendFile("/views/index.html",options);
 })
 
 
-app.post("/",(req,res)=>{
-    var conn=mysql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'',
-        database:'biblioteca'
+app.post("/", (req, res) => {
+    var conn = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'biblioteca'
     });
     conn.connect();
-    var libro={
-        'codigo':req.body.codigo,
-        'titulo':req.body.titulo,
-        'genero':req.body.genero
+    var libro = {
+        'codigo': req.body.codigo,
+        'titulo': req.body.titulo,
+        'genero': req.body.genero
     }
-    var sql=`insert into libros set ?`;
-    conn.query(sql,libro,(err,result,field)=>{
-        if(err){
+    var sql = `insert into libros set ?`;
+    conn.query(sql, libro, (err, result, field) => {
+        if (err) {
             console.log(err);
             res.send("Error conectando a la bbdd");
         }
@@ -38,29 +67,29 @@ app.post("/",(req,res)=>{
         res.redirect("/");
         conn.end();
     })
-     
-    
+
+
 })
 
 
-app.get("/libros",(req,res)=>{
-    var conn=mysql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'',
-        database:'biblioteca'
+app.get("/libros", (req, res) => {
+    var conn = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'biblioteca'
     });
     conn.connect();
-    conn.query("select * from libros",(err,results,fields)=>{
-        let filas="";
+    conn.query("select * from libros", (err, results, fields) => {
+        let filas = "";
         for (let index = 0; index < results.length; index++) {
             const element = results[index];
-            filas+=`<tr><td>${element.codigo}</td>
+            filas += `<tr><td>${element.codigo}</td>
             <td>${element.titulo}</td>
             <td>${element.genero}</td>
             </tr>`;
         }
-        let tabla=`<table>
+        let tabla = `<table>
         <thead>
             <th>Codigo</th>
             <th>Título</th>
@@ -80,5 +109,5 @@ app.get("/libros",(req,res)=>{
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
